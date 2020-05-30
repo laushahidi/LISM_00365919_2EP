1cac35914eab42f36ca4ffa110faadf9cd049c8c
laushahidi <00365919@uca.edu.sv>
2020-05-29 20:54:09 -0600

    Add: fixes almost done

---
+++ Recuperación/Correccion preparcial (1)/Preparcial/Controlador/ControladorPedido.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+
+namespace Preparcial.Controlador
+{
+    public static class ControladorPedido
+    {
+        public static DataTable GetPedidosUsuarioTable(string id)
+        {
+            DataTable pedidos = null;
+
+            try
+            {
+                // Correccion: cambiar 'nombreArticulo' por 'nombreArt'
+                pedidos = ConexionBD.EjecutarConsulta("SELECT p.idPedido, i.nombreArt, p.cantidad, i.precio, (i.precio * p.cantidad) AS total" +
+                                            " FROM PEDIDO p, INVENTARIO i, USUARIO u" +
+                                            " WHERE p.idArticulo = i.idArticulo" +
+                                            " AND p.idUsuario = u.idUsuario" +
+                                            $" AND u.idUsuario = '{id}'");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ha ocurrido un ERROR");
+            }
+
+            return pedidos;
+        }
+
+        public static DataTable GetPedidosTable()
+        {
+            DataTable pedidos = null;
+
+            try
+            {
+                // Correccion: cambiar 'i.nombreArticulo' por 'i.nombreArt'
+                pedidos = ConexionBD.EjecutarConsulta("SELECT p.idPedido, i.nombreArt, p.cantidad, i.precio, (i.precio * p.cantidad) AS total" +
+                                            " FROM PEDIDO p, INVENTARIO i, USUARIO u" +
+                                            " WHERE p.idArticulo = i.idArticulo" +
+                                            " AND p.idUsuario = u.idUsuario");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ha ocurrido un error");
+            }
+
+            return pedidos;
+        }
+
+        public static void HacerPedido(string idUsuario, string idArticulo, string cantidad)
+        {
+            try
+            {
+                ConexionBD.EjecutarComando("INSERT INTO PEDIDO(idUsuario, idArticulo, cantidad) " +
+                    $"VALUES({idUsuario}, {idArticulo}, {cantidad})");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ha ocurrido un error");
+            }
+        }
+    }
+}
---
+++ Recuperación/Correccion preparcial (1)/Preparcial/Vista/FrmPassword.cs
@@ -0,0 +1,54 @@
+﻿using Preparcial.Controlador;
+using Preparcial.Modelo;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Preparcial.Vista
+{
+    public partial class FrmPassword : Form
+    {
+        public FrmPassword()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmPassword_Load(object sender, EventArgs e)
+        {
+            pictureBox1.BackgroundImage = Image.FromFile("../../Recursos/UCA.png");
+            // Correccion: alinear las lineas a la posicion correcta con tab
+            pictureBox1.BackgroundImageLayout = ImageLayout.Stretch;
+            ActualizarControlers();
+        }
+
+        private void ActualizarControlers()
+        {
+        comboBox1.ValueMember = "Contrasena";
+        comboBox1.DataSource = ControladorUsuario.GetUsuarios();
+        comboBox1.DisplayMember = "NombreUsuario";
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            // Correccion: la nueva contrasena no puede quedar vacia
+            if (txtOldPassword.Text.Equals(comboBox1.SelectedValue.ToString()) && txtNewPassword.Text.Length > 0)
+            {       
+                var obtenerUsuario = (Usuario)comboBox1.SelectedItem;
+
+                ActualizarControlers();
+                                
+                ControladorUsuario.ActualizarContrasena(obtenerUsuario.IdUsuario,
+                txtNewPassword.Text);
+            }
+            else
+            // Correccion: cambiar mensaje de error por si la old password esta bien pero el lenght no
+                MessageBox.Show("Por favor verifique que los datos sean correctos");
+        }
+    } 
+}
